deb4acb1612e0b1f8f2396a52c348e12ecaff1bc
joYyHack <agalaktionov5490@gmail.com>
2023-02-08 17:36:34 +0200

    Add goerli network and etherscan support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,22 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-solhint";
 
+import * as dotenv from "dotenv";
+dotenv.config();
+
+require("./tasks/deploy");
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.BOB_PK as string],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
