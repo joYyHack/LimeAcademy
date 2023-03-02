094e2a0369141e1aedd5806e5ac85f9418912334
joYyHack <agalaktionov5490@gmail.com>
2023-03-02 15:41:37 +0200

    Update configs and packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-solhint";
 
 import * as dotenv from "dotenv";
+import { utils } from "ethers/lib";
 dotenv.config();
 
 require("./tasks/deploy");
@@ -10,6 +11,14 @@ require("./tasks/deploy");
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
+    hardhat: {
+      accounts: [
+        {
+          privateKey: process.env.BOB_PK as string,
+          balance: utils.parseEther("100").toString(),
+        },
+      ],
+    },
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.BOB_PK as string],
